a15a9d59f6b8e812a34c74678cc7c8304f253670
ziyanwould <448736378@qq.com>
2019-06-20 22:58:25 +0800

    2-6组件及slot插槽使用

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,36 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="hellow world"/>
     <ev/>
+     <com :age="age" @patch="msg">
+       <!-- 插槽 -->
+       <h1 slot="a" class="class">加首部</h1>
+       <h1 slot="b" class="class">加首部</h1>
+     </com>
   </div>
 </template> 
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import ev from './components/events.vue'
+import com from './components/com.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    ev
+    ev,
+    com
+  },
+  data:function(){
+    return{
+      age:18
+    }
+  },
+  methods:{
+    msg:function(ag){
+      this.age++,
+      window.console.log(ag)
+    }
   }
 }
 </script>
---
+++ src/components/com.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div class="com">
+        <slot name="b"></slot>
+       
+        components {{name}} {{age}}
+        <br>
+        <button class="buttom" type='button'  @click="$emit('patch',34)">发送到父组件</button>
+        <slot name="b"></slot>
+    </div>
+</template>
+
+<script>
+export default{
+ props:['age'],
+ data(){
+     return{
+        name:'com'
+     }
+    
+ }
+ 
+}
+</script>
+
+<style lang="css">
+
+</style>
