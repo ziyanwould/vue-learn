d1ea5460258f131a81db7e8f866dad69816dddf2
ziyanwould <448736378@qq.com>
2019-06-26 17:26:06 +0800

    路由恢复

--- src/router.js
+++ src/router.js
@@ -1,10 +1,10 @@
-
+import Vue from 'vue'
 import VueRouter from 'vue-router'
 import pagea from './pages/pagea.vue'
 import pageb from './pages/pageb.vue'
-
+Vue.use(VueRouter)
 const routes = [
-    { path: '/pagea', component: pagea },
+    { path: '/', component: pagea },
     { path: '/pageb', component: pageb }
 ]
 
