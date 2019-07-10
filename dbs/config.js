3f1b5c3fcee0434a18dafdb94f148322dcddbb38
ziyanwould <448736378@qq.com>
2019-07-10 17:53:38 +0800

    Nuxt.js

--- dbs/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    dbs:'mongodb://127.0.0.1:27017/dbs'
-}
--- dbs/models/person.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose') 
-
-let personSchema = new mongoose.Schema({
-    name:String,
-    age:Number
-})
-
-
-module.exports= mongoose.model('Person',personSchema)
